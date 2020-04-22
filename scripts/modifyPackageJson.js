6b615f787dc1f0a3d1c0775244d02f31422621eb
Ivan Jobs <ruhe@microsoft.com>
2020-04-22 15:33:47 +0800

    merge removeTrailingRc into modifyPackageJson

--- scripts/modifyPackageJson.js
+++ scripts/modifyPackageJson.js
@@ -2,14 +2,21 @@ const fs = require("fs");
 
 console.log(process.argv);
 
+const packageJson = JSON.parse(fs.readFileSync("package.json"));
+const indexOfDash = packageJson.version.indexOf('-');
+
+// remove trailing -rc if it presents.
+if (indexOfDash !== -1) {
+  packageJson.version = packageJson.version.substring(0, indexOfDash);
+}
+
+// modify package.json by key/value pairs.
 const cnt = process.argv.length;
 
 if (cnt % 2 === 1) {
   console.log("Format error, expect even argv length!");
   process.exit(1);
 }
-
-const packageJson = JSON.parse(fs.readFileSync("package.json"));
 
 for (var i = 3; i < cnt; i += 2) {
   packageJson[process.argv[i - 1]] = process.argv[i];
