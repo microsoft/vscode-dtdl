186c26b42f33a25f14660ff7a719f0b6eddd200c
Ivan Jobs <ruhe@microsoft.com>
2020-04-29 16:55:09 +0800

    chore(ut framework): set up for jest and example test

---
+++ jest.config.js
@@ -0,0 +1,52 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // An array of regexp pattern strings used to skip coverage collection
+  coveragePathIgnorePatterns: [
+    "/node_modules/"
+  ],
+
+  // A set of global variables that need to be available in all test environments
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.json"
+    }
+  },
+
+  // An array of file extensions your modules use
+  moduleFileExtensions: [
+    "js",
+    "ts"
+  ],
+
+  // The test environment that will be used for testing
+  testEnvironment: "node",
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: [
+    "**/test/**/*.test.ts"
+  ],
+
+  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "extension.test.ts"
+  ],
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    "\\.ts$": "ts-jest"
+  },
+
+  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  transformIgnorePatterns: [
+    "/node_modules/"
+  ]
+};
---
+++ src/test/example.test.ts
@@ -0,0 +1,3 @@
+test("example test case", () => {
+    expect(1 + 1).toEqual(2);
+});
